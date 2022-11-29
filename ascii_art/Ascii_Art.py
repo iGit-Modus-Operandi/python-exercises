683ba8f9b5a62b7ce83ca3948d2128ea4602011a
iGit-Modus-Operandi <monmon.nietes.acads@gmail.com>
2022-11-29 23:35:58 +0800

    Add some code for the ascii art exercise

---
+++ ascii_art/Ascii_Art.py
@@ -0,0 +1,12 @@
+import os
+import time
+from termcolor import colored
+
+class Canvas:
+    def __init__(self, width, height):
+        self._x = width
+        self._y = height
+        self._canvas = [[' ' for y in range(self._y)] for x in range(self._x)]
+
+    def hitsWall(self, point):
+        return point[0] < 0 or point[0] >= self._x or point[1] < 0 or point[1] >= self._y
