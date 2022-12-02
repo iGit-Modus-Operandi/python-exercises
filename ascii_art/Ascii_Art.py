2a7efad2e0f3a16e4d8b2ca420dcbd80ddcd69b5
iGit-Modus-Operandi <monmon.nietes.acads@gmail.com>
2022-12-02 23:25:16 +0800

    [EXER]: Add functions declarations

--- ascii_art/Ascii_Art.py
+++ ascii_art/Ascii_Art.py
@@ -29,3 +29,24 @@ class TerminalScribe:
         self.mark = '*'
         self.framerate = 0.05
         self.pos = [0, 0]
+
+    def up(self):
+        pos = [self.pos[0], self.pos[1] - 1]
+        if not self.canvas.hitsWall(pos):
+            self.draw(pos)
+
+    def down(self):
+        pos = [self.pos[0], self.pos[1] + 1]
+        if not self.canvas.hitsWall(pos):
+            self.draw(pos)
+
+    def right(self):
+        pos = [self.pos[0] + 1, self.pos[1]]
+        if not self.canvas.hitsWall(pos):
+            self.draw(pos)
+
+    def left(self):
+        pos = [self.pos[0] - 1, self.pos[1]]
+        if not self.canvas.hitsWall(pos):
+            self.draw(pos)
+
